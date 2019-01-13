33955c5e029d6dfd907df5845484330561d49846
CookiesEason <837447352@qq.com>
2019-01-13 10:48:09 +0800

    init dao model mapper for sql demo.

---
+++ src/main/java/com/example/permission/dao/SysRoleUserMapper.java
@@ -0,0 +1,17 @@
+package com.example.permission.dao;
+
+import com.example.permission.model.SysRoleUser;
+
+public interface SysRoleUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysRoleUser record);
+
+    int insertSelective(SysRoleUser record);
+
+    SysRoleUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysRoleUser record);
+
+    int updateByPrimaryKey(SysRoleUser record);
+}
---
+++ src/main/java/com/example/permission/dao/SysUserMapper.java
@@ -0,0 +1,17 @@
+package com.example.permission.dao;
+
+import com.example.permission.model.SysUser;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+}
